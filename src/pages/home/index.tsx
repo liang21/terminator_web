75a428fab6e548276cbd599b0642e544eda29d7a
13537223157@163.com <13537223157@163.com>
2023-08-01 22:57:35 +0800

    feat: optimize home.tsx

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,41 @@
 import React, { useState } from "react";
+import type { MenuProps } from "antd";
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
-  UploadOutlined,
   UserOutlined,
-  VideoCameraOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
 } from "@ant-design/icons";
 import { Layout, Menu, Button, theme } from "antd";
 
 const { Header, Sider, Content } = Layout;
+const items1: MenuProps["items"] = ["1", "2", "3"].map((key) => ({
+  key,
+  label: `nav ${key}`,
+}));
+
+const items2: MenuProps["items"] = [
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+].map((icon, index) => {
+  const key = String(index + 1);
+
+  return {
+    key: `sub${key}`,
+    icon: React.createElement(icon),
+    label: `subnav ${key}`,
+
+    // children: new Array(4).fill(null).map((_, j) => {
+    //   const subKey = index * 4 + j + 1;
+    //   return {
+    //     key: subKey,
+    //     label: `option${subKey}`,
+    //   };
+    // }),
+  };
+});
 
 export default function Home() {
   const [collapsed, setCollapsed] = useState(false);
@@ -17,55 +44,61 @@ export default function Home() {
   } = theme.useToken();
 
   return (
-    <Layout>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
+    <Layout className="layout" style={{ minHeight: "100vh" }}>
+      <Sider
+        width={collapsed ? 50 : 150}
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        onClick={() => setCollapsed(!collapsed)}
+      >
         <div className="demo-logo-vertical" />
         <Menu
-          theme="light"
+          theme="dark"
           mode="inline"
           defaultSelectedKeys={["1"]}
-          items={[
-            {
-              key: "1",
-              icon: <UserOutlined />,
-              label: "nav 1",
-            },
-            {
-              key: "2",
-              icon: <VideoCameraOutlined />,
-              label: "nav 2",
-            },
-            {
-              key: "3",
-              icon: <UploadOutlined />,
-              label: "nav 3",
-            },
-          ]}
+          items={items2}
         />
       </Sider>
+
       <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }}>
+        <Header
+          style={{
+            display: "flex",
+            alignItems: "center",
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          <div className="logo" />
           <Button
             type="text"
             icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
             style={{
               fontSize: "16px",
               width: 64,
               height: 64,
             }}
           />
+          <Menu
+            theme="light"
+            mode="horizontal"
+            defaultSelectedKeys={["2"]}
+            items={items1}
+          />
         </Header>
-        <Content
-          style={{
-            margin: "24px 16px",
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-          }}
-        >
-          Content
-        </Content>
+        <Layout>
+          <Content
+            style={{
+              margin: "3px 5px",
+              padding: 10,
+              minHeight: 280,
+              background: colorBgContainer,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
       </Layout>
     </Layout>
   );
